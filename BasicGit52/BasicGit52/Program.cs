0296f90bc358a90ce5a513d4a9276e81cf4ab643
Herul Syahwandi S <34206362+galontong@users.noreply.github.com>
2021-06-15 09:47:17 +0700

    Create method masing masing

--- BasicGit52/BasicGit52/Program.cs
+++ BasicGit52/BasicGit52/Program.cs
@@ -9,5 +9,60 @@ namespace BasicGit52
             Console.WriteLine("Hello World! dari Herul");
             Console.WriteLine("Hello World! dari Gerald");
         }
+
+        static void MethodHerul()
+        {
+
+        }
+        
+        static void MethodYuzril()
+        {
+
+        }
+
+        static void MethodBoni()
+        {
+
+        }
+
+        static void MethodGerald()
+        {
+
+        }
+
+        static void MethodLingga()
+        {
+
+        }
+
+        static void MethodMahendra()
+        {
+
+        }
+
+        static void MethodMorgan()
+        {
+
+        }
+
+        static void MethodAtha()
+        {
+
+        }
+
+        static void MethodSyifa()
+        {
+
+        }
+
+        static void MethodTaufan()
+        {
+
+        }
+
+        static void MethodFaiz()
+        {
+
+        }
     }
 }
