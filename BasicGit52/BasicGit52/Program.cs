46702c344c838f914264ae4f3a59cc70659d2785
gearstein <48464442+gearstein@users.noreply.github.com>
2021-06-15 09:42:43 +0700

    menambhakan console

--- BasicGit52/BasicGit52/Program.cs
+++ BasicGit52/BasicGit52/Program.cs
@@ -7,6 +7,7 @@ namespace BasicGit52
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! dari Herul");
+            Console.WriteLine("Hello World! dari Gerald");
         }
     }
 }
